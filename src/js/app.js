e8913f73e16a6d567d8a75f833c380fd8fed3a41
jackylu1012 <jacky1352@gmail.com>
2018-11-19 22:44:04 +0800

    v1.0 final

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,21 @@
 import css from '../scss/main.scss';
+
+$(function() {
+    // open menu
+    $('.hamburger-menu').click(function() {
+        $('.menu-content').addClass('fixed');
+    });
+
+    // close menu
+    $('.menu-content .hamburger-menu').click(function() {
+        $('.menu-content').removeClass('fixed');
+    });
+
+    // fancybox - gallery images
+    $('[data-fancybox]').fancybox({
+        buttons : [
+            'close'
+        ],
+        transitionEffect : "slide"
+    });
+});
